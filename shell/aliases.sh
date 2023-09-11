03ed16f378fe54c7779b39ab782b2167b9bf5707
Jose Sanchez <jpsanchez@jpsanchez.localdomain>
2023-09-10 22:44:45 -0600

    Changes in aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,33 +1,31 @@
 # Enable aliases to be sudo’ed
 alias sudo='sudo '
 
+# System
 alias ..="cd .."
-alias ...="cd ../.."
-alias ll="ls -l"
 alias ~="cd ~"
 alias lah="ls -lah"
+alias ll="ls -lh"
 alias dotfiles='cd $DOTFILES_PATH'
 
 # Git
 alias ga="git add ."
 alias gc='git commit'
+alias gs="git status -sb"
 alias gac="git add . && git commit"
 alias gps="git push"
 alias gpl="git pull"
+alias gl="git log"
+alias glp='$DOTLY_PATH/bin/dot git pretty-log'
 
 alias gco="git checkout"
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
-alias gs="git status -sb"
 alias gf="git fetch --all -p"
 alias gpsf="git push --force"
 alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
 
 # Utils
 alias k='kill -9'
-alias i.='(idea $PWD &>/dev/null &)'
-alias c.='(code $PWD &>/dev/null &)'
-alias o.='open .'
 alias up='dot package update_all'
 
 # Own
